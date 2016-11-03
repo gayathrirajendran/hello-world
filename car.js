53ed55783dfa795e6d8b71aeb0019e7e1073a712
Gayathri <gayathriraj.vlr@gmail.com>
2016-11-03 15:50:19 +0530

    Create car.js

---
+++ car.js
@@ -0,0 +1,6 @@
+{           
+            {"model":"Sentra", "doors":4},
+            {"model":"Maxima", "doors":4},               
+            {"model":"Taurus", "doors":4},
+            {"model":"Escort", "doors":4}     
+}
